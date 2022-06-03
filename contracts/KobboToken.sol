f087e1df51cd4f785f0ddfe22a125e200b7316e2
Samuel Ogu <hello@samuelogu.com>
2022-06-03 08:44:48 +0100

    Kobbo token smart contract

--- contracts/KobboToken.sol
+++ contracts/KobboToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract GovernanceToken is ERC20Votes {
-  uint256 public s_maxSupply = 1000000000000000000000000;
+contract KobboToken is ERC20Votes {
+  uint256 public s_maxSupply = 10000000000;
 
-  constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
+  constructor() ERC20("Kobbo Token", "Kobbo") ERC20Permit("KobboToken") {
     _mint(msg.sender, s_maxSupply);
   }
 
