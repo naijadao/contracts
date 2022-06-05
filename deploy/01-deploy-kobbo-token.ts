f422fd9dc0aac1c1ccb281398ea2d6391394207c
Samuel Ogu <hello@samuelogu.com>
2022-06-05 16:57:19 +0100

    token deployment script update

--- deploy/01-deploy-kobbo-token.ts
+++ deploy/01-deploy-kobbo-token.ts
@@ -6,35 +6,35 @@ import { networkConfig, developmentChains } from "../helper-hardhat-config"
 // @ts-ignore
 import { ethers } from "hardhat"
 
-const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployKobboToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // @ts-ignore
   const { getNamedAccounts, deployments, network } = hre
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   log("----------------------------------------------------")
-  log("Deploying GovernanceToken and waiting for confirmations...")
-  const governanceToken = await deploy("GovernanceToken", {
+  log("Deploying Kobbo Token and waiting for confirmations...")
+  const kobboToken = await deploy("KobboToken", {
     from: deployer,
     args: [],
     log: true,
     // we need to wait if on a live network so we can verify properly
     waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
   })
-  log(`GovernanceToken at ${governanceToken.address}`)
+  log(`Kobbo Token at ${kobboToken.address}`)
   if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-    await verify(governanceToken.address, [])
+    await verify(kobboToken.address, [])
   }
   log(`Delegating to ${deployer}`)
-  await delegate(governanceToken.address, deployer)
+  await delegate(kobboToken.address, deployer)
   log("Delegated!")
 }
 
-const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
-  const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress)
+const delegate = async (kobboTokenAddress: string, delegatedAccount: string) => {
+  const governanceToken = await ethers.getContractAt("KobboToken", kobboTokenAddress)
   const transactionResponse = await governanceToken.delegate(delegatedAccount)
   await transactionResponse.wait(1)
   console.log(`Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`)
 }
 
-export default deployGovernanceToken
-deployGovernanceToken.tags = ["all", "governor"]
+export default deployKobboToken
+deployKobboToken.tags = ["all", "governor"]
