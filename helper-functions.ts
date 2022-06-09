db132f96f301ffed4786ed95710af3676f90c469
Samuel Ogu <hello@samuelogu.com>
2022-06-09 14:28:03 +0100

    update verification code

--- helper-functions.ts
+++ helper-functions.ts
@@ -1,12 +1,16 @@
 import { run } from "hardhat"
 
 const verify = async (contractAddress: string, args: any[]) => {
-  console.log("Verifying contract...")
+  console.log(`Verifying contract with address: ${contractAddress}`)
+
   try {
     await run("verify:verify", {
       address: contractAddress,
       constructorArguments: args,
     })
+
+    console.log("Contract successfully verified!");
+
   } catch (e: any) {
     if (e.message.toLowerCase().includes("already verified")) {
       console.log("Already verified!")
