db977e74563e575a3a43e28b1747ccf3c05f325f
Samuel Ogu <hello@samuelogu.com>
2022-06-09 16:00:01 +0100

    setting up roles script

--- deploy/04-setup-governance-contracts.ts
+++ deploy/04-setup-governance-contracts.ts
@@ -1,10 +1,7 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DeployFunction } from "hardhat-deploy/types"
-import verify from "../helper-functions"
-import { networkConfig, developmentChains, ADDRESS_ZERO } from "../helper-hardhat-config"
+import {deployer, ADDRESS_ZERO, GOVERNOR_ADDRESS, TIMELOCK_ADDRESS} from "../helper-hardhat-config"
 import { ethers } from "hardhat"
 
-const setupContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+/*const setupContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // @ts-ignore
   const { getNamedAccounts, deployments, network } = hre
   const { log } = deployments
@@ -30,4 +27,37 @@ const setupContracts: DeployFunction = async function (hre: HardhatRuntimeEnviro
 }
 
 export default setupContracts
-setupContracts.tags = ["all", "setup"]
+setupContracts.tags = ["all", "setup"]*/
+
+
+async function main() {
+
+  const timeLock = await ethers.getContractAt("TimeLock", TIMELOCK_ADDRESS)
+
+  console.log("----------------------------------------------------")
+  console.log("Setting up contracts for roles...")
+
+  const proposerRole = await timeLock.PROPOSER_ROLE()
+  const executorRole = await timeLock.EXECUTOR_ROLE()
+  const adminRole = await timeLock.TIMELOCK_ADMIN_ROLE()
+
+  console.log(`Granting proposer role to ${GOVERNOR_ADDRESS}`)
+  const proposerTx = await timeLock.grantRole(proposerRole, GOVERNOR_ADDRESS)
+  await proposerTx.wait(1)
+
+  console.log(`Granting executor role to ${ADDRESS_ZERO}`)
+  const executorTx = await timeLock.grantRole(executorRole, ADDRESS_ZERO)
+  await executorTx.wait(1)
+
+  console.log(`Revoking admin role for ${deployer}...`)
+  const revokeTx = await timeLock.revokeRole(adminRole, deployer)
+  await revokeTx.wait(1)
+
+  console.log("Roles successfully set up!")
+
+}
+
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
