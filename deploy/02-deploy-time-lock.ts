ed4f87d2b9aad4c56f83fb071d794ad3019e03da
Samuel Ogu <hello@samuelogu.com>
2022-06-09 14:28:20 +0100

    update timelock deployment code

--- deploy/02-deploy-time-lock.ts
+++ deploy/02-deploy-time-lock.ts
@@ -1,9 +1,11 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import verify from "../helper-functions"
-import { networkConfig, developmentChains, MIN_DELAY } from "../helper-hardhat-config"
+import {networkConfig, developmentChains, MIN_DELAY, proposers, deployer} from "../helper-hardhat-config"
+import hre, {ethers} from "hardhat";
+const { network } = hre
 
-const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+/*const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // @ts-ignore
   const { getNamedAccounts, deployments, network } = hre
   const { deploy, log } = deployments
@@ -12,7 +14,7 @@ const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnviro
   log("Deploying TimeLock and waiting for confirmations...")
   const timeLock = await deploy("TimeLock", {
     from: deployer,
-    args: [MIN_DELAY, [], []],
+    args: [MIN_DELAY, proposers, []],
     log: true,
     // we need to wait if on a live network so we can verify properly
     waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
@@ -24,4 +26,33 @@ const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnviro
 }
 
 export default deployTimeLock
-deployTimeLock.tags = ["all", "timelock"]
+deployTimeLock.tags = ["all", "timelock"]*/
+
+async function main() {
+
+  console.log("----------------------------------------------------")
+
+  const Locker = await hre.ethers.getContractFactory("KobboToken");
+
+  console.log("Deploying TimeLock and waiting for confirmations...")
+  const TimeLock = await Locker.deploy(MIN_DELAY, proposers, []);
+  await TimeLock.deployed();
+  console.log("TimeLock successfully deployed:", TimeLock.address);
+
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    await verify(TimeLock.address, [])
+  }
+
+}
+
+const delegate = async (kobboTokenAddress: string, delegatedAccount: string) => {
+  const governanceToken = await ethers.getContractAt("KobboToken", kobboTokenAddress)
+  const transactionResponse = await governanceToken.delegate(delegatedAccount)
+  await transactionResponse.wait(1)
+  console.log(`Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`)
+}
+
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
