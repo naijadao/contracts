5b2e8364485408960c14dacdc894829763674fc8
Samuel Ogu <hello@samuelogu.com>
2022-06-09 18:49:26 +0100

    box contract deployment script

--- deploy/05-deploy-box.ts
+++ deploy/05-deploy-box.ts
@@ -1,4 +1,4 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types"
+/*import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import verify from "../helper-functions"
 import { networkConfig, developmentChains } from "../helper-hardhat-config"
@@ -29,4 +29,33 @@ const deployBox: DeployFunction = async function (hre: HardhatRuntimeEnvironment
 }
 
 export default deployBox
-deployBox.tags = ["all", "box"]
+deployBox.tags = ["all", "box"]*/
+
+import hre from "hardhat";
+import { ethers } from "hardhat"
+import {TIMELOCK_ADDRESS} from "../helper-hardhat-config";
+
+async function main() {
+
+  console.log("----------------------------------------------------")
+
+  const BoxContract = await hre.ethers.getContractFactory("Box");
+
+  console.log("Deploying Box and waiting for confirmations...")
+  const box = await BoxContract.deploy();
+  await box.deployed();
+  console.log("Box successfully deployed:", box.address);
+
+  const boxContract = await ethers.getContractAt("Box", box.address)
+  const timeLock = await ethers.getContractAt("TimeLock", TIMELOCK_ADDRESS)
+
+  console.log("Transfer ownership to TimeLock...");
+  const transferTx = await boxContract.transferOwnership(timeLock.address)
+  await transferTx.wait(1)
+
+}
+
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
