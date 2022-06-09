b853e998691b44822893c616c46ad23aaf0fc3b6
Samuel Ogu <hello@samuelogu.com>
2022-06-09 15:32:35 +0100

    governor deployment arguments

---
+++ arguments/governor.ts
@@ -0,0 +1,15 @@
+import {
+    GOVERNANCE_TOKEN_ADDRESS,
+    QUORUM_PERCENTAGE,
+    TIMELOCK_ADDRESS,
+    VOTING_DELAY,
+    VOTING_PERIOD
+} from "../helper-hardhat-config"
+
+export default [
+    GOVERNANCE_TOKEN_ADDRESS,
+    TIMELOCK_ADDRESS,
+    QUORUM_PERCENTAGE,
+    VOTING_PERIOD,
+    VOTING_DELAY
+];
