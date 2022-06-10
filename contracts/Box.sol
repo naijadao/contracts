b20bcdbd23705bb092d28207ecc8f57d057d4c7d
Samuel Ogu <hello@samuelogu.com>
2022-06-10 08:00:35 +0100

    upgradeable box contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,12 +3,17 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Box is Ownable {
+contract Box is Ownable, Initializable {
   uint256 private value;
 
   // Emitted when the stored value changes
   event ValueChanged(uint256 newValue);
+
+  function initialize(uint256 initialValue) initializer public {
+    value = initialValue;
+  }
 
   // Stores a new value in the contract
   function store(uint256 newValue) public onlyOwner {
